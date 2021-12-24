8a07823926d06559e64c3be521a48b324b3f7b26
joaoivtor <86378145+joaoivtor@users.noreply.github.com>
2021-12-24 19:53:37 -0300

    Add files via upload

--- forca/script.js
+++ forca/script.js
@@ -1,24 +1,54 @@
+// chamando os botão
 const palavra = document.getElementById("palavra");
 const enviaPalavra = document.getElementById("enviaPalavra");
 
 const resposta = document.getElementById("resposta");
 const enviaResposta = document.getElementById("enviaResposta");
 
-function palavraChave(){
+
+
+function criaLista(n){
+    let lista = palavra.value;
+   
+    return lista.substr(n,1)
+}
+
+
+
+
+
+
+// essa função serve para fazer a palavra virar traços
+function palavraChave() {
+
+    console.log(criaLista())
+
+
     let contador = 0;
     let lista = [];
-    
-    if(palavra.value.length <= 9){
-        while(palavra.value.length > contador){
+    //tamanho da palavra
+    if (palavra.value.length <= 15) {
+        //traços
+        while (palavra.value.length > contador) {
             lista.push("_");
             
             contador++;
         }
-    }else{
+        //<><>
+    } else {
         alert("palavra muito grande");
     }
+    palavra.type = "text";
+    //aqui tira a virgula do array
     let tiraVirgula = lista.join(" ");
     palavra.value = tiraVirgula;
+    //<>
 }
+
+function verificaLetra(){
+    
+}
+
+
 
 enviaPalavra.onclick = palavraChave;
